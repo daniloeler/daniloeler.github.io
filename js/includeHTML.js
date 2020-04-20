d1e47ac84589672debff040344517e50c7494bbd
Danilo Eler <daniloelerunesp@gmail.com>
2020-04-20 16:14:06 -0300

    index html

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,46 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8"/>	
-    <title>Danilo Medeiros Eler Website</title>
-</head>
-
-<script>
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
+  /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
+      /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
+          /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }      
+      }
       xhttp.open("GET", file, true);
       xhttp.send();
-      /*exit the function:*/
+      /* Exit the function: */
       return;
     }
   }
-};
-</script>
-
-<body>	
-	<div w3-include-html="header.html">	
-	</div>
-	<script>
-	  includeHTML();
-    </script>
-    <p> Danilo Medeiros Eler Website </p>
-</body>
-</html>
+}
